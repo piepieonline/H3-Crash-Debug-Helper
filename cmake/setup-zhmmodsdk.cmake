58cf1f4441faac5babe6a8dd74d4824b8e62665f
Piepieonline <piepieonline@gmail.com>
2025-02-18 10:27:33 +1100

    CICD

--- cmake/setup-zhmmodsdk.cmake
+++ cmake/setup-zhmmodsdk.cmake
@@ -6,11 +6,10 @@ else()
     cmake_policy(SET CMP0135 NEW)
 
     if (DEFINED ZHMMODSDK_ARTIFACT_ID)
-        set(GITHUB_TOKEN $ENV{secrets.GITHUB_TOKEN})
         FetchContent_Declare(
             ZHMModSDK
             URL https://api.github.com/repos/OrfeasZ/ZHMModSDK/actions/artifacts/${ZHMMODSDK_ARTIFACT_ID}/zip
-            HTTP_HEADER "Authorization: token ${GITHUB_TOKEN}"
+            HTTP_HEADER "Authorization: token $ENV{GITHUB_TOKEN}"
         )
     else()
         FetchContent_Declare(
