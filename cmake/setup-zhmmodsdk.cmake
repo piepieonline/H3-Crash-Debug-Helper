9b7234d16d2d4d7225c2ea8174a792e631def8b5
Piepieonline <piepieonline@gmail.com>
2025-02-18 09:44:23 +1100

    Testing CICD with artifacts

--- cmake/setup-zhmmodsdk.cmake
+++ cmake/setup-zhmmodsdk.cmake
@@ -4,10 +4,19 @@ if (DEFINED ZHMMODSDK_DIR)
 else()
     include(FetchContent)
     cmake_policy(SET CMP0135 NEW)
-    FetchContent_Declare(
-        ZHMModSDK
-        URL https://github.com/OrfeasZ/ZHMModSDK/releases/download/${ZHMMODSDK_VER}/DevPkg-ZHMModSDK.zip
-    )
+
+    if (DEFINED ZHMMODSDK_ARTIFACT_ID)
+        FetchContent_Declare(
+            ZHMModSDK
+            URL https://api.github.com/repos/OrfeasZ/ZHMModSDK/actions/artifacts/${ZHMMODSDK_OVERRIDE}/zip
+            HTTP_HEADER "Authorization: token $ENV{secrets.GITHUB_TOKEN}"
+        )
+    else()
+        FetchContent_Declare(
+            ZHMModSDK
+            URL https://github.com/OrfeasZ/ZHMModSDK/releases/download/${ZHMMODSDK_VER}/DevPkg-ZHMModSDK.zip
+        )
+    endif()
 
     FetchContent_MakeAvailable(ZHMModSDK)
     set(ZHMMODSDK_DIR "${zhmmodsdk_SOURCE_DIR}")
